62b1adf0b747044862134b3fa628104a3394f911
vinmmm <jvinmoore@gmail.com>
2015-03-24 16:55:37 -0400

    changed some class names as ng-class directive doesn't support class names with a dash

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -32,4 +32,21 @@ angular
       });
   });
 
-  
+ 
+ var app = angular.module('progApp', [ngAnimate]);
+ app.controller('barController', function ($scope){
+   $scope.checkoutwrap = 'start-class';
+ });
+ app.animation('.fadeOut', function () {
+  return {
+    enter : function(element, parentElement, afterElement, doneCallback) {},
+    leave : function(element, doneCallback) {},
+    move : function(element, parentElement, afterElement, doneCallback) {},
+    addClass : function(element, className, done) {
+      jQuery(element).animate({opacity: 0}, 3000);
+    },
+    removeClass : function(element, className, done) {
+      jQuery(element).animate({opacity: 1}, 3000);
+    }
+  };
+});
