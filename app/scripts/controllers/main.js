2a2022c5f433763389c8c98d5d6094fa745bf217
vinmmm <jvinmoore@gmail.com>
2015-06-09 18:26:27 -0400

    edits

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,30 +7,17 @@
  * # MainCtrl
  * Controller of the progApp
  */
-angular.module('progApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
 
 // Progress Bar
 
-
 angular.module('progApp')
-  .controller('TimetableCtrl', function TimetableCtrl($scope, $http) {
-  $http.get('src/order_status.json')
-  .success(function(data) { $scope.orderStatus = data.orderStatus; });
-   
-});
+  .controller('MainCtrl', function ($scope, $http) {
+    $scope.awesomeThings = [];  
+    $http.get('src/order_status.json')
+  .success(function(data) { 
+    $scope.orderStatus = data.orderStatus; 
 
-
-angular.module('progApp')
-  .controller('Carlos', function Carlos($scope, $http) {
-  $http.get('src/order_status.json')
-  .success(function(data) { $scope.orderStatus = data.orderStatus; });
+  });
    
 });
 
